ed5635d6cd81560f35b31d136664b427d796537a
Adrian Schultze <adrianschultze@gmail.com>
2020-12-05 21:27:54 +0100

    getting closer

--- fourth.py
+++ fourth.py
@@ -5,22 +5,50 @@ array = f.read().split("\n\n") # Create a list containing all lines
 # something = re.split(r'[\n\n]', lines)
 # array = list(lines)
 # lines = f.read() # Create a list containing all lines
-first = str(array[3])
+# print(array)
+
+def func():
+  blank_array = []
+  for item in array:
+    new_item = item.replace('\n', ' ')
+    blank_array.append(new_item)
+  return blank_array
+
+good_array = func()
+print(good_array)
+
+first = good_array[0]
 print(first)
-# splitFirst = first.split(' ')
-# print(splitFirst)
 
-if (('ecl' in first) and ('hcl' in first) and ('byr' in first) and ('hgt' in first) and ('cid' in first) and ('eyr' in first) and ('pid' in first) and ('iyr' in first)):
-  print("LOL")
-else:
-  print("Expected")
+second = first.split(' ')
+print(second)
 
-if 'iyr' in first:
-  print('HOW')
-else:
-  print("WHY")
+byr_range = range(1920, 2003)
+iyr_range = range(2010, 2021)
+ecl_range = ['amb','blu','brn','gry','grn','hzl','oth']
+# # print(ecl_range)
 
-counter = 0
+
+# if f'iyr:{custom_range}' in test:
+#   print("worked")
+
+# for n in byr_range:
+#   print(n)
+# def func():
+#   counter = 0
+#   for substring in array:
+#     if (('ecl:' in substring) and ('hcl:' in substring) and ('byr:' in substring) and ('hgt:' in substring) and ('cid' in substring) and ('eyr:' in substring) and ('pid:' in substring) and ('iyr:' in substring)):
+#       counter += 1
+#       print(f'Counter: {counter}, ALL FIELS VALID')
+#     elif (('ecl:' in substring) and ('hcl:' in substring) and ('byr:' in substring) and ('hgt:' in substring) and ('eyr' in substring) and ('pid:' in substring) and ('iyr:' in substring)):
+#        counter += 1
+#        print(f'Counter: {counter}, MATCH, ONLY CID Missing')
+#     else:
+#       print('NO MATCH')
+#   return counter
+
+# print(func())
+
 
 
 
